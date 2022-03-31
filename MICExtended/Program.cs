68b1c743b8540819c288b69004f1c354c81295a5
ratanajaya <fakewarszero@gmail.com>
2022-04-01 00:57:47 +0700

    dependency injection

--- MICExtended/Program.cs
+++ MICExtended/Program.cs
@@ -1,18 +1,26 @@
 using MICExtended.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MICExtended
 {
     internal static class Program
     {
+        private static IServiceProvider? ServiceProvider { get; set; }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main() {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            var services = new ServiceCollection();
+            services.AddTransient<AppLogic>();
+            services.AddTransient<IIoWapper, IoWrapper>();
+            services.AddTransient<Form1>();
+
+            ServiceProvider = services.BuildServiceProvider();
+
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1(new AppLogic(new IoWrapper())));
+            Application.Run(ServiceProvider.GetService<Form1>());
         }
     }
 }
