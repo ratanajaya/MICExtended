c06d972f7cf9623cadd3ca0b6af9e683859f9ece
ratanajaya <ratanajayaa@gmail.com>
2022-04-23 13:46:42 +0700

    unit test

--- MICExtended.Test/IntegrationTest.cs
+++ MICExtended.Test/IntegrationTest.cs
@@ -1,4 +1,6 @@
 ﻿using MICExtended;
+using MICExtended.Common;
+using MICExtended.Model;
 using MICExtended.Service;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -9,12 +11,15 @@ namespace MICExtended.Test
     [TestClass]
     public class IntegrationTest
     {
+#pragma warning disable CS8618
         AppLogic _al;
+        string _libPath;
 
         [TestInitialize]
         public void TestInitialize() {
-            var loggerMock = (new Mock<ILogger>()).Object;
+            _libPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "_TestImages");
 
+            var loggerMock = (new Mock<ILogger>()).Object;
             _al = new AppLogic(new IoWrapper(), new ImageCompressor(loggerMock), loggerMock);
         }
 
@@ -23,6 +28,24 @@ namespace MICExtended.Test
             int x = 1 + 1;
 
             Assert.AreEqual(x, 2);
+        }
+
+        [TestMethod]
+        public async Task OpeningNewSrcFolderWithFilter() {
+            var progressMock = new Mock<IProgress<ProgressReport>>().Object;
+
+            var selection = new SelectionCondition {
+                FileTypes = new List<string> { Constant.Extension.JPG, Constant.Extension.PNG },
+                UseMinSize = true,
+                MinSize = 900
+            };
+
+            var files = await _al.GetFileViewModels(_libPath, selection, progressMock);
+            var extensions = files.Select(a => a.Extension).ToList();
+
+            Assert.IsTrue(extensions.Contains(Constant.Extension.JPG));
+            Assert.IsTrue(extensions.Contains(Constant.Extension.PNG));
+            Assert.IsTrue(!extensions.Contains(Constant.Extension.JPEG));
         }
     }
 }
