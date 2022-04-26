be24146cdca47ad7223113a8dcb9c2bd3990eaf5
ratanajaya <ratanajayaa@gmail.com>
2022-04-27 00:52:35 +0700

    Refactor; test

---
+++ MICExtended.Test/UnitTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MICExtended.Common;
+using MICExtended.Model;
+using MICExtended.Service;
+using Moq;
+using Serilog;
+
+namespace MICExtended.Test
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void FooBar() {
+            int x = 1 + 1;
+
+            Assert.AreEqual(x, 2);
+        }
+    }
+}
